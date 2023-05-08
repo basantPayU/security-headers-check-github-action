c335595dca9aae3365886098518b93fb8a931366
Basant <113178058+basantPayU@users.noreply.github.com>
2023-05-08 17:01:19 +0530

    save commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -101,11 +101,11 @@ removeTempFile() {
 if [ $GRADE -lt ${#REQUIRED_HEADERS[@]} ]; then
     printMissingHeaders
     removeTempFile
-    echo "{rating}=failure" >> $GITHUB_OUTPUT
+    echo "rating=failure" >> $GITHUB_OUTPUT
     exit 1
 else 
    echo "${GREEN} All Security Headers are present"
    removeTempFile
-    echo "{rating}=success" >> $GITHUB_OUTPUT
+    echo "rating=success" >> $GITHUB_OUTPUT
    exit 0
 fi
